f9754d07642bccbc4af6ad4065c045c31ad31f1e
Daming <daminglu@live.com>
2015-01-25 00:04:07 -0800

    best_time_to_buy_and_sell_stock i&ii

--- greedy/best_time_to_buy_and_sell_stock_ii.py
+++ greedy/best_time_to_buy_and_sell_stock_ii.py
@@ -1,6 +1,5 @@
 __author__ = 'daming'
 
-
 class Solution:
     # @param prices, a list of integer
     # @return an integer
@@ -9,9 +8,15 @@ class Solution:
             return 0
         cur_min = prices[0]
         cur_max_profit = 0
+        result = 0
         for i in range(1,len(prices)):
             if prices[i] < cur_min:
                 cur_min = prices[i]
             if prices[i] - cur_min > cur_max_profit:
                 cur_max_profit = prices[i] - cur_min
-        return cur_max_profit
+            if prices[i] < prices[i-1]:
+                result += cur_max_profit
+                cur_max_profit = 0
+                cur_min = prices[i]
+        result += cur_max_profit
+        return result
