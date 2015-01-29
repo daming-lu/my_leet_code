a99294e929572f0099ebbf022e3b1bbcd7c81bce
Daming <daminglu@live.com>
2015-01-28 16:06:34 -0800

    Pascal's Triangle II

--- simple_math/pascals_triangle.py
+++ simple_math/pascals_triangle.py
@@ -21,5 +21,24 @@ class Solution:
             ans.append(list(last_row))
         return ans
 
-obj = Solution()
-print obj.generate(5)
+class Solution_II:
+    # @return a list of integers
+    def getRow(self, rowIndex):
+        last_row = [1]
+        if rowIndex == 0:
+            return last_row
+        for i in range(1, 999):
+            cur_row = []
+            for j in range(i+1):
+                if j==0:
+                    cur_row.append(1)
+                    continue
+                if j==i:
+                    cur_row.append(1)
+                    break
+                cur_row.append(last_row[j-1]+last_row[j])
+            last_row = cur_row
+            if i==rowIndex:
+                return last_row
+obj = Solution_II()
+print obj.getRow(4)
