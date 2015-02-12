46fc1faf663152efaf462dbeec6ae5148fcd155c
Daming <daminglu@live.com>
2015-02-11 18:14:59 -0800

    maximum_product_subarray

--- dynamic_programming/maximum_product_subarray.py
+++ dynamic_programming/maximum_product_subarray.py
@@ -4,4 +4,25 @@ class Solution:
     # @param A, a list of integers
     # @return an integer
     def maxProduct(self, A):
-        return 0
+        # max_l = [A[0]]
+        max_inc = [A[0]]
+        min_inc = [A[0]]
+        max_so_far = A[0]
+        for i in range(1, len(A)):
+            inc_with_prev_max = max(A[i] * min_inc[i-1], A[i] * max_inc[i-1], A[i])
+            inc_with_prev_min = min(A[i] * min_inc[i-1], A[i] * max_inc[i-1], A[i])
+
+            cur_max_inc = max(inc_with_prev_max, inc_with_prev_min)
+            cur_min_inc = min(inc_with_prev_max, inc_with_prev_min)
+
+            max_inc.append(cur_max_inc)
+            min_inc.append(cur_min_inc)
+
+            max_so_far = max(max_so_far,cur_max_inc,cur_min_inc)
+
+        return max_so_far
+
+obj = Solution()
+
+
+print obj.maxProduct([9,-3,2,-7,-100])
