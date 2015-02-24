c01e86fba8dde1fe534a7d0b4b520ef9c3a47f49
Daming <daminglu@live.com>
2015-02-24 10:55:33 -0800

    longest_valid_parentheses

---
+++ about_stack/longest_valid_parentheses.py
@@ -0,0 +1,30 @@
+__author__ = 'daming'
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def longestValidParentheses(self, s):
+        cur_max = 0
+        my_stack = []
+        for i in range(0, len(s)):
+            if s[i] == '(':
+                my_stack.append((i,'('))
+            else:
+                if len(my_stack)>0 and my_stack[-1][1] == '(':
+                    last = my_stack.pop()
+                    if len(my_stack) == 0:
+                        cur_max = max(cur_max, i+1)
+                    else:
+                        cur_len = i-my_stack[-1][0]
+                        cur_max = max(cur_max, cur_len)
+                else:
+                    my_stack.append((i,')'))
+
+        return cur_max
+
+obj = Solution()
+
+print obj.longestValidParentheses('(()()')
+
+
+
+
