4b319a7fbe30c494579075f0f9f9b1692f8a2510
Daming <daminglu@live.com>
2015-03-17 07:50:52 -0700

    checkpoint 1

--- Real_Interview_Coding/FourSquare/q2/Find_Endpoint.py
+++ Real_Interview_Coding/FourSquare/q2/Find_Endpoint.py
@@ -3,6 +3,22 @@ import fileinput
 import sys
 
 class Solution:
+    def getPathTokens(self, path):
+        tokens = []
+        last_index = 0
+        i = 1
+        while i < len(path):
+            i = path.find('/', last_index+1)
+            if i==-1:
+                break
+            tokens.append(path[last_index:i])
+            last_index = i
+            i += 1
+        tokens.append(path[last_index:])
+        return tokens
+
+    
+
     def buildHash(self, file_name):
         try:
             file_handle = open(file_name, "r")
@@ -10,11 +26,21 @@ class Solution:
             print >> sys.stderr, "Cannot open config file", file_name
             sys.exit(1)
 
-        self.endpointHash = {}
+        self.pathTokenHash = {}
         for line in iter(file_handle):
             line = line.strip()
             pieces = line.split(' ')
             print pieces
+            dir_tokens = self.getPathTokens(pieces[0])
+            print 'dir_tokens : ', dir_tokens
+            curTokenNode = self.pathTokenHash
+            for token in dir_tokens:
+                if token not in curTokenNode:
+                    curTokenNode[token] = {}
+                curTokenNode = curTokenNode[token]
+            curTokenNode = pieces[1]
+
+
 
     def findEndpoint(self):
         return []
