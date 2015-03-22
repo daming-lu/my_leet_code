94d287f91c191259d6dcd99b09e3771a8398acd9
Daming <daminglu@live.com>
2015-03-22 13:27:40 -0700

    Permutations

---
+++ permutation_and_combination/Permutations.py
@@ -0,0 +1,24 @@
+__author__ = 'daming'
+
+class Solution:
+    def workhorse(self, ans_so_far, num, ans):
+        if len(num) == 0:
+            ans.append(list(ans_so_far))
+            return
+        for i in range(0, len(num)):
+            ans_so_far.append(num[i])
+            tmp = num[i]
+            del num[i]
+            self.workhorse(ans_so_far,num,ans)
+            num.insert(i, tmp)
+            ans_so_far.pop()
+    # @param num, a list of integer
+    # @return a list of lists of integers
+    def permute(self, num):
+        ans = []
+        self.workhorse([],num, ans)
+        return ans
+
+obj = Solution()
+
+print obj.permute([1,2,3])
