d6e7718c5777d8338d737d9665b3d524d3c64285
Daming <daminglu@live.com>
2015-07-12 14:35:18 -0700

    chenyang zenifits.

--- Real_Interview_Coding/zen_chenyang/main.py
+++ Real_Interview_Coding/zen_chenyang/main.py
@@ -1,13 +1,4 @@
 __author__ = 'daming'
-class WordNode:
-    def __init__(self, w):
-        self.word = w
-        self.chain_length = 1
-
-    def __repr__(self):
-        result = '<' + self.word + ',' + str(self.chain_length) + '>'
-        return result
-
 
 def getSmallerWords(word):
     ans = []
@@ -15,43 +6,37 @@ def getSmallerWords(word):
         ans.append(word[0:i]+word[i+1:])
     return ans
 
-def find_longest(my_hash):
+def find_longest(my_hash, length_hash):
     ans = 0
     keys = my_hash.keys()
-    print keys
+    # print keys
     for l in keys:
         if l-1 not in my_hash:
             continue
-        cur_len_word_nodes = my_hash[l]
-        for word_node in cur_len_word_nodes:
-            words_1_char_less = getSmallerWords(word_node.word)
-            cur_chain_length = word_node.chain_length
+        cur_len_words = my_hash[l]
+        for word in cur_len_words:
+            words_1_char_less = getSmallerWords(word)
+            cur_chain_length = length_hash[word]
             for word_1_char_less in words_1_char_less:
-                for word_node_1_less in my_hash[l-1]:
-                    if word_node_1_less.word == word_1_char_less:
-                        cur_chain_length = max(cur_chain_length, word_node_1_less.chain_length+1)
-            word_node.chain_length = cur_chain_length
-            if word_node.chain_length > ans:
-                ans = word_node.chain_length
+                if word_1_char_less in length_hash:
+                    cur_chain_length = max(cur_chain_length, length_hash[word_1_char_less]+1)
+            length_hash[word] = cur_chain_length
+            if cur_chain_length > ans:
+                ans = cur_chain_length
     return ans
 
 def longest_chain(w):
     my_hash = {}
+    length_hash = {}
     for i in range(0, len(w)):
-        print w[i]
-        obj = WordNode(w[i])
+        length_hash[w[i]] = 1
         if len(w[i]) in my_hash:
-            my_hash[len(w[i])].append(obj)
+            my_hash[len(w[i])].append(w[i])
         else:
             my_hash[len(w[i])] = list()
-            my_hash[len(w[i])].append(obj)
+            my_hash[len(w[i])].append(w[i])
+    ans = find_longest(my_hash, length_hash)
 
-    # print my_hash
-
-    # return find_longest(my_hash)
-    ans = find_longest(my_hash)
-
-    # print 'after : ', my_hash
     return  ans
 
 w_orig = ['a','b','ba','bca','bda','bdca']
